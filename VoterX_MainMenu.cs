1af42b67a1ff957b12db9b8c70a6c75ae23d45c6
Mert Gunduz <merttgg@gmail.com>
2021-08-05 00:23:03 +0300

    Code Cleanup

--- VoterX_MainMenu.cs
+++ VoterX_MainMenu.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using OpenQA.Selenium.Firefox;
+using System;
+using System.Data.OleDb;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Threading;
 using System.Windows.Forms;
-using System.IO;
-using System.Data.OleDb;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
 using VoterX.Properties;
 
 namespace VoterX
